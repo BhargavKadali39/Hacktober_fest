b279f4f47e4e9572afc7376acf8774d92e3c5ac1
Bhargav <bhargavkadali39@gmail.com>
2021-10-05 15:35:23 +0530

    Add files via upload

---
+++ ip_addresse_ii.py
@@ -0,0 +1,9 @@
+
+import socket
+
+print(socket.getfqdn())
+name = input('Enter host name of your choise\n')
+socket.sethostname(name)
+# sadly it's only available on UNIX for the moment.
+
+# default LAPTOP-N9O96HRM
